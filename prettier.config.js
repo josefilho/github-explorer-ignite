54fbe5f562e91e39dd8089f54b984be0faebb0de
Jose C. S. Filho <iamjose.filho@gmail.com>
2022-04-13 21:07:13 -0400

    Estilo: Adicionada configurações de estilo

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  singleQuote: true, 
+  trailingComma: 'all',
+	arrowParens: 'avoid',
+  printWidth: 80,
+  tabWidth: 2,
+  semi: true,
+  jsxSingleQuote: true,
+  jsxBracketSameLine: false,
+  bracketSpacing: true,
+}
